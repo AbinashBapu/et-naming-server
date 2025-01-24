5b1f08f9ad86a8d429094f334477765b94d0602c
Abinash Pradhan <abinash.sil668@gmail.com>
2025-01-24 15:47:46 +0530

    configured naming server
      - Configured as naming server
      - used ip address instead of hostname in config server

---
+++ src/test/java/com/rlap/et/namingserver/EtNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rlap.et.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
